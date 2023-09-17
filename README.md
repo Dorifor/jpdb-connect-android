bb168036cf96a3cca47b7f6dd218cf98bde7495d
Mao <doryantilmant25@gmail.com>
2023-09-17 21:03:08 +0200

    Feat: Selectable text Feat: New parsing (Multiple words)

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Jpdb Connect Android
-Android tool to mine words from the Kanji Study app to jpdb.io
+Android tool to mine selected words to jpdb.io (Text selection or Anki format sharing)
 
 ## 🤨 Lore
 Lately I've been using my phone / tablet a lot for immersion, using the wonderful [Kanj Study](https://mindtwisted.com/) as my dictionary. I found it a bit of a shame that I couldn't really mine directly from it. So I made an "app" for that.
@@ -7,6 +7,8 @@ Lately I've been using my phone / tablet a lot for immersion, using the wonderfu
 > I spent 8 hours to do this simple thing help
 
 > I don't know how to make apps
+
+⏩ Flash forward to the next day and I found out it was actually super simple to also just do that from any text selection, so let's go
 
 ## 🐉 Installation
 
@@ -20,7 +22,10 @@ Lately I've been using my phone / tablet a lot for immersion, using the wonderfu
 1. Get your API Key from [the JPDB Settings page](https://jpdb.io/settings)
 2. Input it in the app (first input)
 3. Select your target deck for mining (It has to be created by you)
-4. Select any word in Kanji Study  
+4. Select any text by long pressing (**If you select multiple words, all of them will be added**)
+   1. Tap "Add to JPDB"  
+OR  
+5. Select any word in Kanji Study (or any app that exports words to Anki / shares it) 
    1. "Share example" then select Jpdb Connect Android  
       OR
    2. "Add to Anki" (if you don't have it installed) then select Jpdb Connect Android
@@ -28,11 +33,13 @@ Lately I've been using my phone / tablet a lot for immersion, using the wonderfu
   
 > 👨‍💻 You don't have to start the app to use it every time, just for the initial setup
 
+<details>
+<summary>Screenshots</summary>
 ![screenshot](https://github.com/Dorifor/jpdb-connect-android/assets/28139590/225518ac-0942-401a-b6bf-ca7711fcc8d8)
-
 ![screenshot](https://github.com/Dorifor/jpdb-connect-android/blob/master/Screenshot_20230917_115926.jpg)
+</details>  
 
 ## 🧙‍♂️ Todo  
 * [x] Add indications for fail / success
+* [x] Add a context menu from anywhere to mine selected text (maybe ?)
 * [ ] Add FORQ (Please Kou feed us with the API)
-* [ ] Add a context menu from anywhere to mine selected text (maybe ?)
--- app/src/main/java/com/maooowooo/jpdbconnectandroid/JpdbIntentActivity.kt
+++ app/src/main/java/com/maooowooo/jpdbconnectandroid/JpdbIntentActivity.kt
@@ -9,72 +9,84 @@ import android.widget.Toast
 import com.android.volley.Request.Method
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
+import com.google.gson.Gson
 import org.json.JSONArray
 import org.json.JSONObject
+
+data class ParsePayload(
+    val text: String,
+    val position_length_encoding: String = "utf16",
+    val token_fields: List<String> = emptyList(),
+    val vocabulary_fields: List<String> = listOf("vid", "sid")
+)
+
+data class AddToDeckPayload(
+    val id: Int,
+    val vocabulary: List<List<Long>>
+)
 
 class JpdbIntentActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        var sharedText = intent.getStringExtra(Intent.EXTRA_SUBJECT)
-        if (sharedText != null) {
-            sharedText = sharedText.split(" [")[0]
-        }
-        Log.v("Jpdb", "SHARED : $sharedText")
 
-        if (sharedText != null) {
-            parseWord(sharedText)
+        var textToAdd = ""
+
+        if (intent.action == Intent.ACTION_SEND) {
+            val sharedText = intent.getStringExtra(Intent.EXTRA_SUBJECT) ?: return
+            textToAdd = sharedText.split(" [")[0]
+        } else if (intent.action == Intent.ACTION_PROCESS_TEXT) {
+            textToAdd = intent.getStringExtra(Intent.EXTRA_PROCESS_TEXT).toString()
         }
 
+        parseText(textToAdd)
         finish()
     }
 
-    fun parseWord(word: String) {
-        val payloadString = "{\n" +
-                "\t\"text\": \"$word\",\n" +
-                "\t\"position_length_encoding\": \"utf16\",\n" +
-                "\t\"token_fields\": [],\n" +
-                "\t\"vocabulary_fields\": [\n" +
-                "\t\t\"vid\",\n" +
-                "\t\t\"sid\"\n" +
-                "\t]\n" +
-                "}"
-
-        val payloadJson = JSONObject(payloadString)
+    private fun parseText(text: String) {
+        val payload = ParsePayload(text)
+        val payloadJson = JSONObject(Gson().toJson(payload))
         Log.v("Jpdb", payloadJson.toString(4))
 
         sendRequest(Method.POST, "https://jpdb.io/api/v1/parse", payloadJson) { response ->
-            val wordVid = ((response["vocabulary"] as JSONArray)[0] as JSONArray)[0]
-            val wordSid = ((response["vocabulary"] as JSONArray)[0] as JSONArray)[1]
-
-            addToDeck(wordVid as Number, wordSid as Number, word)
+            val parsedWords = response["vocabulary"] as JSONArray
+            addToDeck(parsedWords, text)
         }
     }
 
-    fun addToDeck(wordVid: Number, wordSid: Number, word: String) {
+    private fun addToDeck(vocabularyList: JSONArray, text: String) {
         val sharedPref = this.getSharedPreferences("jpdb_prefs", Context.MODE_PRIVATE)
         val deckId = sharedPref?.getString("target_deck_id", null)
 
         if (deckId == null) {
-            val toast = Toast.makeText(this, "You need to set a target deck in the settings.", Toast.LENGTH_SHORT)
-            toast.show()
+            showToast("You need to set a target deck in the settings.", Toast.LENGTH_LONG)
             return
         }
 
-        val payloadString = "{\n" +
-                "\t\"id\": $deckId,\n" +
-                "\t\"vocabulary\": [[$wordVid, $wordSid]]\n" +
-                "}"
+        val parseVocabList = arrayListOf<List<Long>>()
 
-        val payloadJson = JSONObject(payloadString)
+        (0 until vocabularyList.length()).forEach { i ->
+            val parsedWordEntry = vocabularyList[i] as JSONArray
+            val wordVid = parsedWordEntry[0].toString().toLong()
+            val wordSid = parsedWordEntry[1].toString().toLong()
+            parseVocabList.add(listOf(wordVid, wordSid))
+        }
+
+        val payload = AddToDeckPayload(deckId.toInt(), parseVocabList)
+        val payloadJson = JSONObject(Gson().toJson(payload))
+
         Log.v("Jpdb", payloadJson.toString(4))
 
-        sendRequest(Method.POST, "https://jpdb.io/api/v1/deck/add-vocabulary", payloadJson) { response ->
-            val toast = Toast.makeText(this, "「$word」 added to target deck !", Toast.LENGTH_SHORT)
-            toast.show()
+        sendRequest(
+            Method.POST,
+            "https://jpdb.io/api/v1/deck/add-vocabulary",
+            payloadJson
+        ) {
+            val toastText = if (text.length > 10) text.substring(0..10) + "..." else text
+            showToast("「$toastText」 added to target deck !")
         }
     }
 
-    fun sendRequest(
+    private fun sendRequest(
         method: Int,
         endpoint: String,
         payload: JSONObject? = null,
@@ -84,15 +96,15 @@ class JpdbIntentActivity : Activity() {
         val apiKey = sharedPref?.getString("api_key", null)
 
         if (apiKey == null) {
-            val toast = Toast.makeText(this, "You need to set your api key in the settings.", Toast.LENGTH_SHORT)
-            toast.show()
+            showToast("You need to set your api key in the settings.", Toast.LENGTH_LONG)
             return
         }
 
         val queue = Volley.newRequestQueue(this)
         val jsonObjectRequest = object : JsonObjectRequest(
             method, endpoint, payload,
-            { response -> if (onCompleteCallback != null) onCompleteCallback(response)
+            { response ->
+                if (onCompleteCallback != null) onCompleteCallback(response)
             },
             { error ->
                 Log.e("Jpdb", error.toString())
@@ -109,5 +121,10 @@ class JpdbIntentActivity : Activity() {
             }
         }
         queue.add(jsonObjectRequest)
+    }
+
+    private fun showToast(text: String, length: Int = Toast.LENGTH_SHORT) {
+        val toast = Toast.makeText(this, text, length)
+        toast.show()
     }
 }
